d4c1c79df576b7b35e41f53fcae5641ca6b2ce7b
Marko <marko.bachl@gmail.com>
2024-05-24 08:31:59 +0200

    Update run_from_github.R

--- run_from_github.R
+++ run_from_github.R
@@ -1,2 +1,4 @@
 if (!require("shiny")) install.packages("shiny")
+if (!require("httr2")) install.packages("httr2")
+if (!require("tidyverse")) install.packages("tidyverse")
 shiny::runGitHub( "shiny_prompt_checker", "bachl")
